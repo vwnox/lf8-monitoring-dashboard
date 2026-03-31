89232e98c635342186e1b4b0beec3c04d29217f2
Murtaza18-AURA <03murtaza11@gmail.com>
2026-03-31 13:01:19 +0200

    deploy ready

--- app/web.py
+++ app/web.py
@@ -20,6 +20,9 @@ def home():
     <p>RAM: {system['memory_usage']} %</p>
     """
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
     
